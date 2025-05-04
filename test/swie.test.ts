1ac2797dccd17291a883783643a0d844ce80d150
Vijayabaskar <vj2k02@gmail.com>
2025-05-04 23:21:59 +0530

    initial setup

---
+++ test/swie.test.ts
@@ -0,0 +1,11 @@
+// Your test user is already created
+import { describe, expect, it } from 'vitest';
+// const { headers, user } = await signInWithTestUser();
+
+describe("My Plugin", () => {
+ it("should do something cool", async () => {
+  // const result = await client.myPlugin.doSomethingCool();
+
+  // expect(result).toBe(true);
+ });
+});
